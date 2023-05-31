14d3881c70f36f464c127b8cae862363d3400f02
Didaca <deni_sk8@abv.bg>
2023-05-31 19:06:28 +0300

    Add widgets

--- main.py
+++ main.py
@@ -68,8 +68,8 @@ def result():
 
 window = Tk()
 window.title("CALCULATOR")
-window.geometry("400x400")
-window.config(bg="darkgray")
+window.geometry("320x350")
+window.config(bg="#33415c", pady=16, padx=21)
 
 first_input = ""
 second_input = ""
@@ -78,58 +78,78 @@ next_input = False
 
 value = StringVar()
 
-label = Label(window, textvariable=value, background="white", width=39, height=2)
+label = Label(window, textvariable=value)
+label.config(bg="#33415c", width=24, height=2, font=40, anchor="e", foreground="#fff")
 label.pack()
 
-frame = Frame(window, background="darkgray")
+print(label.configure().keys())
+
+frame = Frame(window)
+frame.config(bg="#33415c")
 frame.pack()
 
-button_1 = Button(frame, text="1", width=5, height=2, font=40, command=lambda: calculation(1))
+button_1 = Button(frame, text="1", command=lambda: calculation(1))
+button_1.config(width=5, height=2, font=40, borderwidth=2, relief="groove")
 button_1.grid(column=0, row=2)
 
-button_2 = Button(frame, text="2", width=5, height=2, font=40, command=lambda: calculation(2))
+button_2 = Button(frame, text="2", command=lambda: calculation(2))
+button_2.config(width=5, height=2, font=40, borderwidth=2, relief="groove")
 button_2.grid(column=1, row=2)
 
-button_3 = Button(frame, text="3", width=5, height=2, font=40, command=lambda: calculation(3))
+button_3 = Button(frame, text="3", command=lambda: calculation(3))
+button_3.config(width=5, height=2, font=40, borderwidth=2, relief="groove")
 button_3.grid(column=2, row=2)
 
-button_4 = Button(frame, text="4", width=5, height=2, font=40, command=lambda: calculation(4))
+button_4 = Button(frame, text="4", command=lambda: calculation(4))
+button_4.config(width=5, height=2, font=40, borderwidth=2, relief="groove")
 button_4.grid(column=0, row=1)
 
-button_5 = Button(frame, text="5", width=5, height=2, font=40, command=lambda: calculation(5))
+button_5 = Button(frame, text="5", command=lambda: calculation(5))
+button_5.config(width=5, height=2, font=40, borderwidth=2, relief="groove")
 button_5.grid(column=1, row=1)
 
-button_6 = Button(frame, text="6", width=5, height=2, font=40, command=lambda: calculation(6))
+button_6 = Button(frame, text="6", command=lambda: calculation(6))
+button_6.config(width=5, height=2, font=40, borderwidth=2, relief="groove")
 button_6.grid(column=2, row=1)
 
-button_7 = Button(frame, text="7", width=5, height=2, font=40, command=lambda: calculation(7))
+button_7 = Button(frame, text="7", command=lambda: calculation(7))
+button_7.config(width=5, height=2, font=40, borderwidth=2, relief="groove")
 button_7.grid(column=0, row=0)
 
-button_8 = Button(frame, text="8", width=5, height=2, font=40, command=lambda: calculation(8))
+button_8 = Button(frame, text="8", command=lambda: calculation(8))
+button_8.config(width=5, height=2, font=40, borderwidth=2, relief="groove")
 button_8.grid(column=1, row=0)
 
-button_9 = Button(frame, text="9", width=5, height=2, font=40, command=lambda: calculation(9))
+button_9 = Button(frame, text="9", command=lambda: calculation(9))
+button_9.config(width=5, height=2, font=40, borderwidth=2, relief="groove")
 button_9.grid(column=2, row=0)
 
-button_0 = Button(frame, text="0", width=5, height=2, font=40, command=lambda: calculation(0))
+button_0 = Button(frame, text="0", command=lambda: calculation(0))
+button_0.config(width=5, height=2, font=40, borderwidth=2, relief="groove")
 button_0.grid(column=1, row=3)
 
-button_clear = Button(frame, text="C", width=5, height=2, font=40, command=clear)
+button_clear = Button(frame, text="C", command=clear)
+button_clear.config(width=5, height=2, font=40, borderwidth=2, fg="#33415c", relief="groove")
 button_clear.grid(column=0, row=3)
 
-button_plus = Button(frame, text="+", width=5, height=2, font=40, command=lambda: calculation("+"))
+button_plus = Button(frame, text="+", command=lambda: calculation("+"))
+button_plus.config(width=5, height=2, font=40, borderwidth=2, relief="groove")
 button_plus.grid(column=3, row=0)
 
-button_minus = Button(frame, text="-", width=5, height=2, font=40, command=lambda: calculation("-"))
+button_minus = Button(frame, text="-", command=lambda: calculation("-"))
+button_minus.config(width=5, height=2, font=40, borderwidth=2, relief="groove")
 button_minus.grid(column=3, row=1)
 
-button_multiple = Button(frame, text="*", width=5, height=2, font=40, command=lambda: calculation("*"))
+button_multiple = Button(frame, text="x", command=lambda: calculation("*"))
+button_multiple.config(width=5, height=2, font=40, borderwidth=2, relief="groove")
 button_multiple.grid(column=3, row=2)
 
-button_divide = Button(frame, text="/", width=5, height=2, font=40, command=lambda: calculation("/"))
+button_divide = Button(frame, text="/", command=lambda: calculation("/"))
+button_divide.config(width=5, height=2, font=40, borderwidth=2, relief="groove")
 button_divide.grid(column=3, row=3)
 
-button_result = Button(frame, text="=", width=5, height=2, font=40, command=result)
+button_result = Button(frame, text="=", command=result)
+button_result.config(width=5, height=2, font=40, borderwidth=2, bg="#f3dfa2", relief="groove")
 button_result.grid(column=2, row=3)
 
 window.mainloop()
